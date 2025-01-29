17bb111ced85cfec0ac626a33b0d4c5baeedef5d
Abuubakar-Ciise <abuubakarciise4@gmai.com>
2025-01-29 23:18:23 +0300

    Controllers

---
+++ blog_app/lib/controllers/new_post_controller.dart
@@ -0,0 +1,26 @@
+import 'package:blog_app/controllers/PostController.dart';
+import 'package:blogapp/controllers/PostController.dart';
+import 'package:get/get.dart';
+import 'package:blogapp/services/api_service.dart';
+
+class NewPostController extends GetxController {
+  final PostController postController = Get.find<PostController>();
+  Future<void> addPost(String title, String content, String imagePath) async {
+    try {
+      final response = await ApiService().createPostWithImage(title, content, imagePath);
+
+      if (response['success'] == true) {
+        Get.snackbar('Success', 'Post created successfully!',
+            snackPosition: SnackPosition.TOP);
+            postController.fetchPosts();
+            Get.offAllNamed('/home');
+            
+      } else {
+        throw Exception(response['message'] ?? 'Failed to create post');
+      }
+    } catch (e) {
+      Get.snackbar('Error', 'An error occurred: $e',
+          snackPosition: SnackPosition.TOP);
+    }
+  }
+}
